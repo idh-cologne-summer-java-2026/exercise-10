c8fa7e0d6b503b489c711604494eaede77786177
A.A <167234045+aamirbek@users.noreply.github.com>
2026-07-03 11:15:17 +0200

    Aufgabe 10

--- src/idh/java/RPSTournament.java
+++ src/idh/java/RPSTournament.java
@@ -62,8 +62,23 @@ public class RPSTournament {
 	 * player remains, return their name.
 	 */
 	public String runTournament() {
-		// TODO: implement
-		return null;
+	    while (players.size() > 1) {
+	        String player1 = players.poll();
+	        String player2 = players.poll();
+
+	        String winner = playRound(player1, player2);
+
+	        if (winner == null) {
+	            System.out.println("Tie! Both players return to the queue.");
+	            players.add(player1);
+	            players.add(player2);
+	        } else {
+	            System.out.println("Winner: " + winner);
+	            players.add(winner);
+	        }
+	    }
+
+	    return players.peek();
 	}
 
 	/**
@@ -76,8 +91,20 @@ public class RPSTournament {
 	 * (tie).
 	 */
 	public String playRound(String player1, String player2) {
-		// TODO: implement
-		return null;
+	    Move move1 = randomMove();
+	    Move move2 = randomMove();
+
+	    System.out.println(player1 + " (" + move1 + ") vs " + player2 + " (" + move2 + ")");
+
+	    if (move1 == move2) {
+	        return null;
+	    }
+
+	    if (beats(move1, move2)) {
+	        return player1;
+	    } else {
+	        return player2;
+	    }
 	}
 
 	/**
@@ -95,7 +122,8 @@ public class RPSTournament {
 	 * TODO: implement the three winning cases.
 	 */
 	public static boolean beats(Move move1, Move move2) {
-		// TODO: implement
-		return false;
+	    return (move1 == Move.ROCK && move2 == Move.SCISSORS)
+	        || (move1 == Move.PAPER && move2 == Move.ROCK)
+	        || (move1 == Move.SCISSORS && move2 == Move.PAPER);
 	}
 }
